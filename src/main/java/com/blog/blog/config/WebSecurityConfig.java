ddce901dd7bf6bc35c99a9a9129d1abe9e2ee665
Aleksa Kundačina <aleksa.kundacina@energosoft.rs>
2024-08-23 15:23:58 +0200

    start security

---
+++ src/main/java/com/blog/blog/config/WebSecurityConfig.java
@@ -0,0 +1,49 @@
+package com.blog.blog.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    private static final String[] WHITELIST = {
+            "/register",
+            "/h2-console/*",
+            "/"
+    };
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers(WHITELIST).permitAll()
+                        .requestMatchers(HttpMethod.GET, "/posts/*").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin((form) -> form
+                        .loginPage("/login")
+                        .loginProcessingUrl("/login")
+                        .usernameParameter("email")
+                        .passwordParameter("password")
+                        .defaultSuccessUrl("/", true)
+                        .failureUrl("/login?error=true")
+                        .permitAll()
+                ) .logout(logout ->
+                        logout
+                                .logoutUrl("/logout")
+                                .logoutSuccessUrl("/login?logout")
+                                .permitAll()
+                );
+
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+
+        return http.build();
+    }
+
+}
